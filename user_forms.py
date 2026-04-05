d0ae150bfe459871b2de50d9f70067c8a671850a
Felice Forby <morinoko@hey.com>
2026-04-05 10:21:26 -0400

    Add WTForms for Registration and Login

--- user_forms.py
+++ user_forms.py
@@ -1,6 +1,38 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, validators
 from wtforms.validators import DataRequired
+
+class RegistrationForm(FlaskForm):
+    first_name = StringField('First name', [
+        validators.DataRequired(),
+        validators.Length(max=50, message="First name cannot exceed 50 characters.")
+    ])
+    last_name = StringField('Last name', [
+        validators.DataRequired(),
+        validators.Length(max=50, message="Last name cannot exceed 50 characters.")
+    ])
+    email = StringField('Email', [
+        validators.DataRequired(),
+        validators.Email(message="Please enter a valid email address."),
+        validators.Length(max=120, message="Email cannot exceed 120 characters.")
+    ])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.Length(min=7, message="Password must be at least 7 characters long.")
+    ])
+    password_confirmed = PasswordField('Password Confirmation', [
+        validators.DataRequired(),
+        validators.EqualTo('password', message="Passwords do not match.")
+    ])
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', [
+        validators.DataRequired(),
+        validators.Email(message="Please enter a valid email address.")
+    ])
+    password = PasswordField('Password', [
+        validators.DataRequired()
+    ])
 
 class EditUserForm(FlaskForm):
     first_name = StringField('First name', validators=[DataRequired()])
@@ -8,4 +40,3 @@ class EditUserForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
     new_password = PasswordField('New Password', validators=[DataRequired()])
     new_password_confirmed = PasswordField('New Password Confirmation', validators=[DataRequired()])
-    
