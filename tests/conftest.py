a83df313b9dd9ae9fcf5ad14bd35bfc796b2775d
nabl2290 <nabl2290@colorado.edu>
2026-04-12 12:46:50 -0400

    Update tests/conftest.py
    remove todo

--- tests/conftest.py
+++ tests/conftest.py
@@ -56,7 +56,6 @@ def create_child(app):
 
 @pytest.fixture
 def create_feeding_event(app):
-    #TODO: need to add a child befor creating a feeding event, otherwise the foreign key constraint will fail.
     def _create_feeding_event(
             child_id=1,
             timestamp=datetime(2024, 6, 1, 8, 0),
